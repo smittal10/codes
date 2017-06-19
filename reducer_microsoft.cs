5fcc6e7be28f5a5aa442c4a9173f8f55ea1e6463
smittal10 <iec2014071@iiita.ac.in>
2017-06-20 00:31:50 +0530

    My first command line commit :p

---
+++ reducer_microsoft.cs
@@ -0,0 +1,15 @@
+double
+
+#CS
+Dictionary<double,List<string>> dict = new Dictionary<double,List<string>>();
+List<double> ls;
+List<int> y;
+double duration=0;
+foreach(Row row in input){
+	ls.Add(Stime+duration);
+	duration=row["StateDuration"];
+
+	y.Add
+	Dict
+}
+#ENDOCS
