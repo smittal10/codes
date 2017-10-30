827a1c8207c264406e3a522508a2d5b459551134
smittal10 <iec2014071@iiita.ac.in>
2017-10-30 20:08:03 +0530

    fixing errors in lcs

--- lcs.cpp
+++ lcs.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 #define MAX 1024
 int LCS[MAX][MAX];
@@ -7,18 +8,20 @@ int LCS[MAX][MAX];
 int find_lcs(string x,string y){
 	int m= x.length();
 	int n = y.length();
-	for(int j=0;j<n+1;j++){
-	LCS[0][j]=0;
-	LCS[j][m]=0
-
+	int i,j;
+	for(j=0;j<n+1;j++){
+	LCS[m][j]=0;
 	}
-	for(int i=m-1;i>=0;i--){
-		for(int j=n-1;j>=0;j--){
-			if(x[i]==y[i]){
+	for(j=0;j<m+1;j++){
+		LCS[j][n]=0;
+	}
+	for(i=m-1;i>=0;i--){
+		for(j=n-1;j>=0;j--){
+			if(x[i]==y[j]){
 				LCS[i][j]=1+LCS[i+1][j+1];
 			}
 			else{
-				LCS[i][j]=MAX(LCS[i+1][j],LCS[i][j+1]);
+				LCS[i][j] = max(LCS[i+1][j],LCS[i][j+1]);
 			}
 		}
 	}
@@ -28,7 +31,8 @@ int find_lcs(string x,string y){
 
 int main(){
 	string x,y;
-	cin>>x>>y;
+	cin>>y;
+	cin>>x;
 	cout<<find_lcs(x,y);
 	return 0;
 }
